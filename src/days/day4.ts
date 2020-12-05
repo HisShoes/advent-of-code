50723fda8909094b12cd575d7d48d9e5c787fba7
Ryan Simpson <ryan999000999@hotmail.co.uk>
2020-12-05 03:24:50 +0000

    day4 typing

--- src/days/day4.ts
+++ src/days/day4.ts
@@ -2,20 +2,22 @@ import { Context } from '../context';
 import { Day } from '../types';
 
 type Passport = {
-  byr?: string;
-  iyr?: string;
-  eyr?: string;
-  hgt?: string;
-  hcl?: string;
-  ecl?: string;
-  pid?: string;
+  byr: string;
+  iyr: string;
+  eyr: string;
+  hgt: string;
+  hcl: string;
+  ecl: string;
+  pid: string;
 };
+
+type ValidationFunction = (p: Passport) => Boolean;
 
 const checkNumberMinMax = (num, min, max) => parseInt(num) >= min && parseInt(num) <= max;
 
 const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
-const validations = [
+const validations: ValidationFunction[] = [
   ({ byr }) => checkNumberMinMax(byr, 1920, 2002),
   ({ iyr }) => checkNumberMinMax(iyr, 2010, 2020),
   ({ eyr }) => checkNumberMinMax(eyr, 2020, 2030),
@@ -29,7 +31,7 @@ const validations = [
         return false;
     }
   },
-  ({ hcl }) => hcl.slice(0, 1) && hcl.slice(1).length === 6 && parseInt(hcl.slice(1)) !== NaN,
+  ({ hcl }) => hcl.slice(0, 1) === '#' && hcl.slice(1).length === 6 && parseInt(hcl.slice(1)) !== NaN,
   ({ ecl }) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].indexOf(ecl) > -1,
   ({ pid }) => pid.length === 9 && parseInt(pid) !== NaN,
 ];
@@ -39,14 +41,14 @@ const validatePassportKeys = (passport) => requiredFields.every((key) => passpor
 const validatePassportValues = (passport) => validations.every((validation) => validation(passport));
 
 export const day4 = (context: Context): Day => {
-  const passports: Passport[] = context
+  const passports = context
     .getDayInput(4, '\r\n\r\n')
     .map((passport) =>
       passport
         .replace(/\r\n/g, ' ')
         .split(' ')
         .map((pair) => pair.split(':'))
-        .reduce((acc, cur) => ({ ...acc, [cur[0]]: cur[1] }), {}),
+        .reduce((acc, cur) => ({ ...acc, [cur[0]]: cur[1] }), <Passport>{}),
     )
     .filter(validatePassportKeys);
 
