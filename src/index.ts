9ba6ae6b25b5075c5970f98ea785210249ca258a
Ryan Simpson <ryan999000999@hotmail.co.uk>
2020-12-06 12:53:30 +0000

    day6 solution

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import { day2 } from './days/day2';
 import { day3 } from './days/day3';
 import { day4 } from './days/day4';
 import { day5 } from './days/day5';
+import { day6 } from './days/day6';
 
 const context: Context = createContext();
 
-const days = [day1(context), day2(context), day3(context), day4(context), day5(context)];
+const days = [day1, day2, day3, day4, day5, day6];
 
 days.forEach((day) => {
-  console.log(day.part1());
-  console.log(day.part2());
+  console.log(day(context).part1());
+  console.log(day(context).part2());
 });
