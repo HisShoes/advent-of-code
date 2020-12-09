83f1a95e3f7afccf40681dc6214d4d925299e76a
Ryan Simpson <ryan999000999@hotmail.co.uk>
2020-12-09 19:09:36 +0000

    day9 solution

--- src/context/index.ts
+++ src/context/index.ts
@@ -3,8 +3,29 @@ import * as fs from 'fs';
 const getDayInput = (dayNumber: number, splitBy = '\n'): string[] =>
   fs.readFileSync(`./inputs/day${dayNumber}`, 'utf8').split(splitBy);
 
+const findSumToTarget = (data, target) => {
+  const step = (start, end): number[] => {
+    if (data[start] + data[end] === target) {
+      return [data[start], data[end]];
+    }
+
+    if (start > end) {
+      return [];
+    }
+
+    if (data[start] + data[end] > target) {
+      return step(start, end - 1);
+    }
+
+    return step(start + 1, end);
+  };
+
+  return step(0, data.length - 1);
+};
+
 export const createContext = () => ({
   getDayInput,
+  findSumToTarget,
 });
 
 export type Context = ReturnType<typeof createContext>;
--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ import { day5 } from './days/day5';
 import { day6 } from './days/day6';
 import { day7 } from './days/day7';
 import { day8 } from './days/day8';
+import { day9 } from './days/day9';
 
 const context: Context = createContext();
 
-const days = [day1, day2, day3, day4, day5, day6, day7, day8];
+const days = [day1, day2, day3, day4, day5, day6, day7, day8, day9];
 
 days.forEach((day) => {
-  console.log(day(context).part1());
-  console.log(day(context).part2());
+  const d = day(context);
+  console.log(d.part1());
+  console.log(d.part2());
 });
