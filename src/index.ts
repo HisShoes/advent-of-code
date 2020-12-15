e451f75d1e634952aad2a3f684685ae40f2ddc7e
Ryan Simpson <ryan999000999@hotmail.co.uk>
2020-12-15 09:39:46 +0000

    day 10 solution

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Context, createContext } from './context';
 import { day1 } from './days/day1';
+import { day11 } from './days/day11';
+import { day10 } from './days/day10';
 import { day2 } from './days/day2';
 import { day3 } from './days/day3';
 import { day4 } from './days/day4';
@@ -11,7 +13,7 @@ import { day9 } from './days/day9';
 
 const context: Context = createContext();
 
-const days = [day1, day2, day3, day4, day5, day6, day7, day8, day9];
+const days = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10];
 
 days.forEach((day) => {
   const d = day(context);
